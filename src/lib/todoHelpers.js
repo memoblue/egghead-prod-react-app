e6b655dbb5676a39b11aeef6a6c9246d7a8796e4
memo <me@memo.blue>
2017-04-28 23:01:26 -0400

    add helpers

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,1 +1,14 @@
 export const generateId = () => Math.floor(Math.random()*100000);
+
+export const findById = (list, id) => list.find(item => item.id === id);
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !!todo.isComplete});
+
+export const updateTodo = (list, updatedItem) => {
+  const updatedIndex = list.findIndex(item => item.id === updatedItem.id);
+  return [
+    ...list.slice(0, updatedIndex),
+    updatedIndex,
+    ...list.slice(updatedIndex+1)
+  ];
+};
