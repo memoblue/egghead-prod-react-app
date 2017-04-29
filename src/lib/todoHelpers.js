26d05a7a245dcb7bdb43ff0e16ee8f0813733369
memo <me@memo.blue>
2017-04-29 11:03:04 -0400

    fix stupid bugs in helper

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -2,13 +2,13 @@ export const generateId = () => Math.floor(Math.random()*100000);
 
 export const findById = (list, id) => list.find(item => item.id === id);
 
-export const toggleTodo = (todo) => ({...todo, isComplete: !!todo.isComplete});
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete});
 
 export const updateTodo = (list, updatedItem) => {
   const updatedIndex = list.findIndex(item => item.id === updatedItem.id);
   return [
     ...list.slice(0, updatedIndex),
-    updatedIndex,
+    updatedItem,
     ...list.slice(updatedIndex+1)
   ];
 };
