50798bcb31d355a880edf7dcc39a4788c56ee50a
memo <me@memo.blue>
2017-05-07 18:35:14 -0400

    add Router component (but doesn't work as expected)

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 export class Link extends Component {
-  handleClick = (e) => {
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func,
+  }
+
+  handleClick(e) {
     e.preventDefault();
-    history.pushState(null, '', this.props.to)
+    this.context.linkHandler(this.props.to)
   }
 
   render() {
-    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+    const activeClass = this.context.route === this.props.to ? 'active' : '';
+    return <a href="#" className={activeClass} onClick={this.handleClick}>{this.props.children}</a>
   }
+}
+
+Link.propTypes = {
+  to: React.PropTypes.string.isRequired
 }
